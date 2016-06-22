969847f80051ecc678a5c022afaa2a83924d0f4a
Jim Wrubel <jwrubel@gmail.com>
2016-06-21 20:10:58 -0400

    Let's just get the beer names since they would match even if the brewery was included

--- builder.py
+++ builder.py
@@ -19,10 +19,8 @@ with open("eng.user_words",'w') as f:
     for category in categories:
         brewery_list = rb.brewers_by_alpha(category)
         for brewery in brewery_list:
-            f.writelines(brewery.name.encode('utf8') + "\n")
             beer_list = brewery.get_beers()
             for beer in beer_list:
                 #index the beer name without the bewery too
                 beer_name_without_brewery = strip_brewery_name(brewery.name, beer.name)
-                f.writelines(beer.name.encode('utf8') + "\n")
                 f.writelines(beer_name_without_brewery.encode('utf8') + "\n")
