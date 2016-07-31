0c1e40f187cf8949c99ee2206a57bad76424fb20
Jim Wrubel <jwrubel@gmail.com>
2016-07-31 10:23:24 -0400

    lots of tweaks to the ratebeer scraper. Looking good, Billy Ray

--- builder.py
+++ builder.py
@@ -1,7 +1,12 @@
 import ratebeer
 import string
 import unicodecsv as csv
+import re
 from io import BytesIO
+
+def strip_parens(beer):
+    p = re.compile(r'\([^)]*\)')
+    return re.sub(p, '', beer)
 
 def strip_brewery_name(brewery_name, beer_name):
     brewery_word_list = brewery_name.split()
@@ -13,37 +18,60 @@ def brewery_name_field(brewery):
     val = getattr(brewery, 'name', 'RateBeer does not have this field filled out for this brewery')
     return val
 
-def beer_description_field(beer):
-    val = getattr(beer, 'description', 'no description is available')
-    return val
 
 categories = []
 categories.append("0-9")
 for letter in string.ascii_uppercase:
     categories.append(letter)
 
-unique_beer_names = []
+unique_beer_words = []
 
 rb = ratebeer.RateBeer()
 
 mapping = open('mapping.csv','w')
 kv = csv.writer(mapping, quoting=csv.QUOTE_MINIMAL)
-kv.writerow( ('beer name','url','description','full name') )
+kv.writerow( ('beer_name','url','brewery_name') )
 
 with open("eng.user_words",'w') as f:
     for category in categories:
         brewery_list = rb.brewers_by_alpha(category)
         for brewery in brewery_list:
-            f.writelines(brewery.name.encode('utf8') + "\n")
-            beer_list = brewery.get_beers()
-            for beer in beer_list:
-                try:
-                    #index the beer name without the bewery
-                    beer_name_without_brewery = strip_brewery_name(brewery.name, beer.name)
-                    if beer_name_without_brewery not in unique_beer_names:
-                        f.writelines(beer_name_without_brewery.encode('utf8') + "\n")
-                        unique_beer_names.append(beer_name_without_brewery)
-                    kv.writerow( (beer_name_without_brewery.encode('utf8'),beer.url,beer_description_field(beer).encode('utf8'),brewery_name_field(brewery).encode('utf8')) )
-                except Exception: 
-                  pass
+            try:
+                brewery_name = strip_parens(brewery.name)
+                brewery_name = brewery_name.strip()
+                brewery_words = brewery_name.split(" ")
+                for word in brewery_words:
+                    if word not in unique_beer_words:
+                        f.writelines(word.encode('utf8') + "\n")
+                        unique_beer_words.append(word)
+                beer_list = brewery.get_beers()
+                for beer in beer_list:
+                    try:
+                        # get rid of parens
+                        beer_name = strip_parens(beer.name)
+                        # if it contains a / only use what's to the right of it
+                        beer_name = beer_name.rsplit('/', 1)[-1]
+                        #leading and trailing spaces
+                        beer_name = beer_name.strip()
+                        # if the beer has fewer than three words when the
+                        # brewery name is removed and there is no dash character,
+                        # use the full name otherwise use the beer name
+                        # without the brewery
+                        beer_without_brewery = strip_brewery_name(brewery_name, beer_name)
+                        beer_without_brewery_word_count = beer_without_brewery.split(" ")
+                        if (len(beer_without_brewery_word_count) > 2) and ("-" not in beer_without_brewery):
+                            beer_name = beer_without_brewery
+                        beer_words = beer_name.split(" ")
+                        for word in beer_words:
+                            # ignore weird garbage characters in the name
+                            if len(word) > 1:
+                                if word not in unique_beer_words:
+                                    f.writelines(word.encode('utf8') + "\n")
+                                    unique_beer_words.append(word)
+                        kv.writerow( (beer_name.encode('utf8'),beer.url,brewery_name.encode('utf8')) )
+                    except Exception, e:
+                        print str(e)
+                        pass
+            except Exception:
+              pass
 mapping.close()
